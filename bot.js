f73eef50d1f8d419c6f0e61fe33990f5e827b542
wilburforce83 <wilburforce83@gmail.com>
2021-08-05 13:50:57 +0100

    Updating to add relay-raspberry npm

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 // Boilerplate developed by Will Sheare for Aquaco Water Recycling Limited. It is untested, but a basic boilerplate to worked on to help with the development
 // or a basic smart attenuation system
 
+// to be used with https://www.npmjs.com/package/relay-raspberry 
+
 var global = require('./globals');
 
 var rss = require('./lib/metofficeRSS');
@@ -8,5 +10,5 @@ var rss = require('./lib/metofficeRSS');
 
 setTimeout(function () { // run checking cycle
   rss.checkWeather()
-  setInterval(rss.checkWeather, 1000 * 60 * 60 * global.frequency); // converts minutes into milliseconds for working with node.
+  setInterval(rss.checkWeather, 1000 * 60  * global.frequency); // converts minutes into milliseconds for working with node.
 }, 5000);
